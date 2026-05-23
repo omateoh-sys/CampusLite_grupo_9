8752619bdfa031c66cb521b6af50a670f8cdef73
omateo <omateoh@miumg.edu.gt>
2026-05-22 22:29:37 -0600

    Feat: Implementar estructura base de las ventanas de la interfaz gráfica y clase Main

---
+++ src/campuslite/ui/VentanaEstudiantes.java
@@ -0,0 +1,10 @@
+package campuslite.ui;
+
+public class VentanaEstudiantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/campuslite/ui/VentanaMaestros.java
@@ -0,0 +1,10 @@
+package campuslite.ui;
+
+public class VentanaMaestros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
