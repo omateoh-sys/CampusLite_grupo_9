6b08e186c2e04652a3973632e32440cbd68855ef
omateo <omateoh@miumg.edu.gt>
2026-05-22 22:27:44 -0600

    Feat: Agregar estructura base para la capa de persistencia y manejo de archivos

---
+++ src/campuslite/persistencia/Bitacora.java
@@ -0,0 +1,5 @@
+package campuslite.persistencia;
+
+public class Bitacora {
+
+}
---
+++ src/campuslite/persistencia/GestorCampus.java
@@ -0,0 +1,10 @@
+package campuslite.persistencia;
+
+public class GestorCampus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
